f5164df83a3d12f92f5195f4b2372cd11967099c
Gautham0718 <gautham.09may2002@gmail.com>
2023-02-25 00:04:50 +0530

    @ 12 pm saturday feb 25

--- src/HomeComponents/founder/founder.jsx
+++ src/HomeComponents/founder/founder.jsx
@@ -8,7 +8,7 @@ export default function founder() {
   return (
     <div className="founder">
         <div className="lll">
-        <Carousel fade>
+        <Carousel fade style={{width:'100%', height:'100%'}}>
       <Carousel.Item>
         <img
           className="d-block w-100"
